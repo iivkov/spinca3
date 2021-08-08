fb5f32ae9fa9bb6a091bea1b97f40b2b1f76021c
Ivan <ivan.ivkovic1996@gmail.com>
2021-08-08 19:23:57 +0200

    npm i @angular-devkit/schematics i page register

--- Bite/src/app/app-routing.module.ts
+++ Bite/src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {
     path: '**',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   }
+
 ];
 @NgModule({
   imports: [
