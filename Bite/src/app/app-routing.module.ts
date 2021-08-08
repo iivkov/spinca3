804267cc55483e471317a9f5b4efc91855a14279
Ivan <ivan.ivkovic1996@gmail.com>
2021-08-08 20:07:14 +0200

    Page za registraciju

--- Bite/src/app/app-routing.module.ts
+++ Bite/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '**',
+    path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  },  {
+  },
+  {
     path: 'register',
     loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   }
